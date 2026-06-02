e922b68d957906a44b254cbeab52ae6a702a908d
Rodrigo <rtrigosorojas@gmail.com>
2026-06-01 21:57:30 -0500

    actualizacion con el header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 ﻿import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet],
+  imports: [CommonModule, HeaderComponent, RouterLink, RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
