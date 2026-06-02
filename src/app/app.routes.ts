1abe10af6cc3ce6bb74a94f86145fef12db31ff8
SandroCy6 <diazsandro844@gmail.com>
2026-06-01 21:18:39 -0500

    add contacto feature with form and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Contacto } from './features/contacto/contacto';
+export const routes: Routes = [
+  { path: 'contacto', component: Contacto}
+];
