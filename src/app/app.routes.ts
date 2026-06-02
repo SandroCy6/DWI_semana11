228b96f4a0f3bab75dfe5dc71a3486e452ab407b
SandroCy6 <diazsandro844@gmail.com>
2026-06-01 21:48:54 -0500

    add sedes feature with component, template, styles, and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { Contacto } from './features/contacto/contacto';
+import { Sedes } from './features/sedes/sedes';
 export const routes: Routes = [
-  { path: 'contacto', component: Contacto}
+  { path: 'contacto', component: Contacto},
+  { path: 'sedes', component: Sedes },
 ];
